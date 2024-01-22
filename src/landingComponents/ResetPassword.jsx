0e5ff33e08801d0f97548e88763f3451d5e6925d
RamyaShanthi-27 <ramyashanthi@gmail.com>
2024-01-22 17:28:35 +0530

    changes

--- src/landingComponents/ResetPassword.jsx
+++ src/landingComponents/ResetPassword.jsx
@@ -28,7 +28,7 @@ export default function ResetPassword() {
     try {
       // Replace ":randomString" with the extracted random string
       const response = await axios.post(
-        `/api/reset-password/${params.token}`,
+        `/api/resetpassword/${params.token}`,
         values
       );
       console.log("response", response);
@@ -83,8 +83,7 @@ export default function ResetPassword() {
       <div className="container min-vh-100 d-flex align-items-center justify-content-center py-5 py-md-0">
         <div className="content row gx-0">
           <div className="col-md-5">
-            <div className="image-container text-white h-100 d-flex align-items-center justify-content-center flex-column p-5 text-center"
-             style={{ backgroundImage: `url(./image/Gold.jpeg)` }}>
+            <div className="image-container text-white h-100 d-flex align-items-center justify-content-center flex-column p-5 text-center">
               <h2 className="mb-3 fw-bold">Need An Account ?</h2>
               <p>Register Here</p>
               <Link to="/register">
