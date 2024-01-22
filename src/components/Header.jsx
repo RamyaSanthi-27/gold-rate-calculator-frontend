aeded1bb035bedabd866f45b03fe3b1e4e01600e
RamyaShanthi-27 <ramyashanthi@gmail.com>
2024-01-22 17:09:30 +0530

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,11 @@ const Header = () => {
     <>
       <header className="shadow-sm bg-dark">
         <div className="container d-flex flex-warp justify-content-center pt-2 mb-2">
-         
+          <Link
+            to="/today"
+            className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-white"
+            title="Gold price today"
+          >
             <img
               src="https://pricegold.net/images/logo.svg"
               height="70"
@@ -41,7 +45,7 @@ const Header = () => {
             />
             <span className="fs-4">Gold price</span>
             <span className="fs-7 ms-1 border-bottom">Today</span>
-       
+          </Link>
 
           <div
             className="col-12 col-lg-8 mb-3 pt-2 mb-lg-0 text-lg-end text-center"
